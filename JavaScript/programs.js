50f8f324cbf123d605fc8473c1f6f533834b7020
Rukeshr <63596884+Rukeshr@users.noreply.github.com>
2023-04-21 18:20:22 +0530

    Update

--- JavaScript/programs.js
+++ JavaScript/programs.js
@@ -1,1 +1,13 @@
+//JavaScript function that reverse a number
 
+function Reverse(num){
+    console.log(num.toString().split("").reverse().join(''));  //Convert number to string by toString method and split by quotes and reverse it and join it quotes.
+}
+Reverse(1234);
+
+//JavaScript function that returns a passed string with letters in alphabetical order
+
+const order = (str) => {
+    console.log(str.toLowerCase().split('').sort().join('')); //convert to lowercase(captial letter wont sort in mixed case) and split sort it and join. 
+}
+order('Rukesh')
