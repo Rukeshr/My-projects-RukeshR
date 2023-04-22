8221f97f8cd7b9d9eb20dae2d6c462deaa5b5610
Rukeshr <63596884+Rukeshr@users.noreply.github.com>
2023-04-22 16:50:47 +0530

    Update programs.js

--- JavaScript/programs.js
+++ JavaScript/programs.js
@@ -11,3 +11,26 @@ const order = (str) => {
     console.log(str.toLowerCase().split('').sort().join('')); //convert to lowercase(captial letter wont sort in mixed case) and split sort it and join. 
 }
 order('Rukesh')
+
+//JavaScript function that accepts a string as a parameter and counts the number of vowels within the string
+const vowel = (str) => {
+    console.log(str.match(/[aeiou]/gi).length);
+}
+
+vowel("my name is Rukesh")
+
+    // Another function 
+
+    function vowels(str) {
+        const vl = 'aeiouAEIOU'
+        let count = 0;
+
+        for (let x = 0; x < str.length; x++) {
+            if (vl.indexOf(str[x]) !== -1) {
+                count += 1;
+            }
+        }
+        console.log(count);
+    }
+
+    (vowels("My name is rukesh"))
